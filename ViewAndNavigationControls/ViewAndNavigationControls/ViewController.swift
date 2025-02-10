18c39f72ae7631af38f2e32b6554f2e44e73cf7b
eduard Alexi cardona Grajales <edwardcardona1972@gmail.com>
2025-02-10 17:46:53 +0100

    Curso de Udemy

---
+++ ViewAndNavigationControls/ViewAndNavigationControls/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  ViewAndNavigationControls
+//
+//  Created by Eduard Alexis Cardona Grajales on 6/2/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var myView: UIView!
+    @IBOutlet weak var myImage: UIImageView!
+    @IBOutlet weak var myFakeView: UIView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+   
+    @IBAction func myButtonAction(_ sender: Any) {
+        myFakeView.isHidden = true
+    }
+}
+    
+
+
+
