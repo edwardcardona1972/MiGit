ee5c8cacf015fac964a035add42d979e0a0fb88e
eduard Alexi cardona Grajales <edwardcardona1972@gmail.com>
2025-02-17 10:03:16 +0100

    creacion de las ViewController por color

--- Navegacion/Navegacion/ViewController.swift
+++ Navegacion/Navegacion/ViewController.swift
@@ -12,11 +12,28 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
     }
     
-
+    @IBAction func BotonNegro(_ sender: Any) {
+        print("Boton Negro")
+        performSegue(withIdentifier: "VCNegro", sender: self)
+        
+        
+    }
     
     
+    @IBAction func BotonRosa(_ sender: Any) {
+        print("Boton Rosa")
+        
+        performSegue(withIdentifier: "VCRosa", sender: self)
+    }
     
+    
+    @IBAction func BotonVerde(_ sender: Any) {
+        print("Boton Verde")
+        performSegue(withIdentifier: "VCVerde", sender: self)
+        
+    }
     
 }
---
+++ Navegacion/Navegacion/ViewControllerVerde.swift
@@ -0,0 +1,29 @@
+//
+//  ViewControllerVerde.swift
+//  Navegacion
+//
+//  Created by Eduard Alexis Cardona Grajales on 17/2/25.
+//
+
+import UIKit
+
+class ViewControllerVerde: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
