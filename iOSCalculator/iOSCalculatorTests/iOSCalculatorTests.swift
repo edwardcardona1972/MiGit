c5413ea5b766550d76f0a119e2f558f448148418
eduard Alexi cardona Grajales <edwardcardona1972@gmail.com>
2025-02-26 14:54:06 +0100

    Creacion de XIV desde la pantalla q se crea en iOS por defecto

---
+++ iOSCalculator/iOSCalculatorTests/iOSCalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOSCalculatorTests.swift
+//  iOSCalculatorTests
+//
+//  Created by Eduard Alexis Cardona Grajales on 26/2/25.
+//
+
+import Testing
+@testable import iOSCalculator
+
+struct iOSCalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ iOSCalculator/iOSCalculatorUITests/iOSCalculatorUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  iOSCalculatorUITestsLaunchTests.swift
+//  iOSCalculatorUITests
+//
+//  Created by Eduard Alexis Cardona Grajales on 26/2/25.
+//
+
+import XCTest
+
+final class iOSCalculatorUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
