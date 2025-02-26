4e716e53dad94c722abaeaf6696bed032d8f37e0
eduard Alexi cardona Grajales <edwardcardona1972@gmail.com>
2025-02-26 14:55:39 +0100

    Prepacion para el inicio de programacion de la calculadora

--- iOSCalculator/iOSCalculator/Controller/Home/HomeViewController.swift
+++ iOSCalculator/iOSCalculator/Controller/Home/HomeViewController.swift
@@ -7,7 +7,21 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+final class HomeViewController: UIViewController {
+    
+    // MARK: - Initialización
+    
+    init(){
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Lyfe Cycle
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
